dda0c9f0eed8f0de6b79a13904ab66a03acf8210
Nathan Toups <n@rjrbt.io>
2021-12-05 22:43:08 -0700

    wip

---
+++ offset/src/routes/__layout.svelte
@@ -0,0 +1,41 @@
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+	/>
+	<!-- CSS Reset -->
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+	/>
+	<!-- Milligram CSS -->
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"
+	/>
+</svelte:head>
+
+<main class="container">
+	<slot />
+</main>
+<footer class="container"><a href="https://funcimp.biz/">funcimp</a></footer>
+
+<style>
+	:global(body, html, #svelte) {
+		width: 100vw;
+		min-height: 100vh;
+	}
+	:global(#svelte) {
+		display: flex;
+		flex-direction: column;
+	}
+	main {
+		flex: 1;
+		margin-top: 3rem;
+	}
+	footer {
+		margin-top: auto;
+		font-size: 0.8em;
+		opacity: 0.5;
+	}
+</style>
--- offset/src/routes/index.svelte
+++ offset/src/routes/index.svelte
@@ -1,9 +1,18 @@
 <script>
-	import { XoShiRo128PlusPlus } from 'prng-xoshiro';
-	import { gen_rand_seed, nextRGB, nextBinary } from '../lib/utils.js';
-	let rand = new XoShiRo128PlusPlus(gen_rand_seed());
+	import { XoShiRo128PlusPlus } from 'prng-xoshiro/src/xoshiro128';
+	import { gen_rand_seed, nextRGB, nextBinary } from '$lib/utils';
+	import { createQueryStore } from '$lib/query';
+
+	let rand = new XoShiRo128PlusPlus(5555555);
 	console.log(rand.nextNumber());
+	console.log(nextRGB(rand));
+
+	const seed = createQueryStore('s');
 </script>
 
 <h1>Welcome to SvelteKit</h1>
+
+{$seed}
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input bind:value={$seed} />
