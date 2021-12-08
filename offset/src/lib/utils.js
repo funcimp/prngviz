537c3a204a65c6345bcf3fc7036471c855be2f6d
Nathan Toups <n@rjrbt.io>
2021-12-07 23:11:44 -0700

    wip: rgb generator

--- offset/src/lib/utils.js
+++ offset/src/lib/utils.js
@@ -1,23 +1,54 @@
 
 
-export function gen_rand_seed() {
-    return Math.floor(Math.random() * 0xFFFFFFFFFFFFF)
+// rand_seed generates a 32 bit uint as a padded hex eg (07d92112)
+export function rand_seed() {
+    return (Math.floor(Math.random() * 0xFFFFFFFFFFFFF) >>> 0).toString(16).padStart(8, '0');
 }
 
-export function nextRGB(r) {
-    let h = r.nextNumber().toString(16)
-    return h.substring(h.length - 6,)
+export function generator(seed) {
+    let next = xorshift32(fromHex(seed))
+    let state = new Uint32Array([next(), next(), next(), next()]);
+    return xoshiro128pp(state)
 }
 
-export function nextBinary(r) {
-    let h = r.nextNumber().toString(2)
-    return h.substring(h.length - 1,)
+export const toBinary = n => n & 1
+export const fromHex = h => parseInt(h, 16)
+export const toRGB = n => (n >>> 0).toString(16).padStart(6, '0').substring(2)
+
+function xorshift32(a) {
+    return () => {
+        a ^= a << 13; a ^= a >>> 17; a ^= a << 5;
+        return Math.floor((a >>> 0) / 4294967296 * 0xFFFFFFFFF) >>> 0;
+    }
 }
 
-export function toHex(n) {
-    return n.toString(16)
+// this algorithm is based on this c code:
+// https://prng.di.unimi.it/xoshiro128plusplus.c
+
+const rotl = (x, k) => (x << k) | (x >> (32 - k))
+
+function xoshiro128pp(s) {
+    return () => {
+        const result = rotl(s[0] + s[3], 7) + s[0];
+        const t = s[1] << 9;
+        s[2] ^= s[0];
+        s[3] ^= s[1];
+        s[1] ^= s[2];
+        s[0] ^= s[3];
+        s[2] ^= t;
+        s[3] = rotl(s[3], 11);
+        return result & 0x7FFFFFFF;
+    }
 }
 
-export function fromHex(h) {
-    return parseInt(h, 16)
+
+let seed = rand_seed()
+let next = generator(seed)
+for (let i = 0; i < 1_000_000; i++) {
+    let rgb = 0
+    for (let j = 0; j < 24; j++) {
+        let n = next()
+        rgb = (rgb << 1) | toBinary(next())
+    }
+    console.log(rgb)
 }
--- offset/src/lib/xoshiro128pp.js
+++ offset/src/lib/xoshiro128pp.js
@@ -1,4 +1,3 @@
-
 // this algorithm is based on this c code:
 // https://prng.di.unimi.it/xoshiro128plusplus.c
 
@@ -7,27 +6,52 @@ function rotl(x, k) {
 }
 
 function xoshiro128pp(s) {
-    const result = rotl(s[0] + s[3], 7) + s[0];
-    const t = s[1] << 9;
-    s[2] ^= s[0];
-    s[3] ^= s[1];
-    s[1] ^= s[2];
-    s[0] ^= s[3];
-    s[2] ^= t;
-    s[3] = rotl(s[3], 11);
-    return result;
+    return () => {
+        const result = rotl(s[0] + s[3], 7) + s[0];
+        const t = s[1] << 9;
+        s[2] ^= s[0];
+        s[3] ^= s[1];
+        s[1] ^= s[2];
+        s[0] ^= s[3];
+        s[2] ^= t;
+        s[3] = rotl(s[3], 11);
+        return result & 0x7FFFFFFF;
+    }
 }
 
-
-let state = new Uint32Array([3595898852, 4290026455, 1751400157, 3565898852]);
-
-export function nextRGB(n) {
-    let h = n.toString(16)
-    return h.substring(h.length - 6,)
+function xorshift32(a) {
+    return () => {
+        a ^= a << 13; a ^= a >>> 17; a ^= a << 5;
+        return Math.floor((a >>> 0) / 4294967296 * 0xFFFFFFFFF) >>> 0;
+    }
 }
 
-for (let i = 0; i < 1_000_000; i++) {
-    let n = xoshiro128pp(state)
-    // console.log(n)
-    console.log(parseInt(nextRGB(n), 16))
+// console.log(x)
+let next = xorshift32(0xcf0805a5)
+let y = next()
+console.log(y, y.toString(2).length,)
+// console.log(next())
+// console.log(next())
+// console.log(next())
+
+// uint64_t next() {
+// 	uint64_t z = (x += 0x9e3779b97f4a7c15);
+// 	z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+// 	z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+// 	return z ^ (z >> 31);
+// }
+// rand_seed generates 
+export function rand_seed() {
+    return (Math.floor(Math.random() * 0xFFFFFFFFFFFFF) >>> 0).toString(16).padStart(8, '0');
 }
+
+console.log(rand_seed())
+
+
+let state = new Uint32Array([1512351235, 23529358023587, 52035925, 25825825]);
+
+
+// for (let i = 0; i < 1; i++) {
+//     let n = xoshiro128pp(state)
+//     console.log(n & 0x7FFFFFFF)
+// }
